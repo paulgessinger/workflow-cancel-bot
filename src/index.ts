cb033c3304804fbaff90944bacd0e455d05a6f21
Paul Gessinger <hello@paulgessinger.com>
2020-06-12 15:15:17 +0200

    add basic version

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,56 @@
-import { Application } from 'probot' // eslint-disable-line no-unused-vars
+import { Application, GitHubAPI } from 'probot' // eslint-disable-line no-unused-vars
 
 export = (app: Application) => {
-  app.on('issues.opened', async (context) => {
-    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
-    await context.github.issues.createComment(issueComment)
-  })
-  // For more information on building apps:
-  // https://probot.github.io/docs/
+  app.on('pull_request.synchronize', async (context) => {
+    // console.log(context);
+    // const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    // await context.github.issues.createComment(issueComment)
 
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+    const { name, payload, github } = context;
+    console.log(name);
+
+    const {pull_request: { head: {ref, sha}}} = payload;
+
+    console.log(ref, sha);
+
+  // console.log({ eventName, sha, headSha, branch, owner, repo, GITHUB_RUN_ID });
+  let workflow_ids: number[] = [];
+
+  // get workflow IDs
+
+  let workflows = await github.actions.listRepoWorkflows(context.repo());
+
+  for (let wf of workflows.data.workflows) {
+    // console.log(wf);
+    workflow_ids.push(wf.id);
+  }
+
+  console.log(`Found workflow_id: ${JSON.stringify(workflow_ids)}`);
+
+  await Promise.all(workflow_ids.map(async (workflow_id) => {
+    try {
+      const { data } = await github.actions.listWorkflowRuns(context.repo({
+        workflow_id,
+        ref
+      }));
+      console.log(`Found ${data.total_count} runs total.`);
+      // console.log(data.workflow_runs);
+      const runningWorkflows = data.workflow_runs.filter(
+        workflow => workflow.head_sha !== sha && workflow.status !== 'completed'
+      );
+      console.log(`Found ${runningWorkflows.length} runs in progress.`);
+      for (const {id, head_sha, status} of runningWorkflows) {
+        console.log('Cancelling another run: ', {id, head_sha, status});
+        const res = await github.actions.cancelWorkflowRun(context.repo({
+          run_id: id
+        }));
+        console.log(`Cancel run ${id} responded with status ${res.status}`);
+      }
+    } catch (e) {
+      const msg = e.message || e;
+      console.log(`Error while cancelling workflow_id ${workflow_id}: ${msg}`);
+    }
+  }));
+
+  });
+};
